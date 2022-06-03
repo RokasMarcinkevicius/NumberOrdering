0e64c7039a46287fc22b30cc841273e09e8371e8
Rokas Marcinkevicius <rokas.m97@gmail.com>
2022-06-03 19:34:54 +0300

    Logger exceptions for file service

--- NumberOrdering.Services/Services/FileService.cs
+++ NumberOrdering.Services/Services/FileService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using NumberOrdering.Repository.Models;
 using NumberOrdering.Services.Interfaces;
 using System;
@@ -10,34 +11,49 @@ namespace NumberOrdering.Services.Services
 {
     public class FileService : IFileService
     {
-        public bool SaveToFile(string fileName, List<int> numbers)
+        private readonly ILogger<FileService> _logger;
+
+        public FileService(ILogger<FileService> logger)
+        {
+            _logger = logger;
+        }
+        
+        public bool SaveToFile(List<int> numbers, string fileName)
         {
             try
             {
                 File.WriteAllText(fileName, JsonSerializer.Serialize<List<int>>(numbers));
                 return true;
             }
-            // TODO add logger here
-            catch (Exception)
+            catch (Exception exception)
             {
+                _logger.LogError(exception, "Saving to file failed");
                 return false;
             }
         }
 
         public List<int> LoadLatestFile(string fileName = null)
         {
-            // TODO add logger here
-            if(fileName == null)
+            try
             {
-                var configuration = ConfigurationOperations.ReadConfiguration();
-                ConfigurationOperations.SaveChanges(configuration);
+                if(fileName == null)
+                {
+                    var configuration = ConfigurationOperations.ReadConfiguration();
+                    ConfigurationOperations.SaveChanges(configuration);
 
-                return JsonSerializer.Deserialize<List<int>>(File.ReadAllText(configuration.ConnectionStrings.LastFile));
+                    return JsonSerializer.Deserialize<List<int>>(File.ReadAllText(configuration.ConnectionStrings.LastFile));
+                }
+                else
+                {
+                    return JsonSerializer.Deserialize<List<int>>(File.ReadAllText(fileName));
+                }
             }
-            else
+            catch (Exception exception)
             {
-                return JsonSerializer.Deserialize<List<int>>(File.ReadAllText(fileName));
+                _logger.LogError(exception, "File loading failed");
+                return null;
             }
+            
         }
 
         public string ConvertFileToString(IFormFile file)
