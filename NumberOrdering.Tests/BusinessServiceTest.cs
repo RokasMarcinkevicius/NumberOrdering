ca3873e54947dc963257801c137e549be3066834
Rokas Marcinkevicius <rokas.m97@gmail.com>
2022-06-04 15:18:29 +0300

    Placeholder for business service tests

---
+++ NumberOrdering.Tests/BusinessServiceTest.cs
@@ -0,0 +1,60 @@
+namespace NumberOrdering.Tests;
+
+public class BusinessServiceTest
+{
+    // For public List<int> ImportNumberList(IFormFile file)
+    [Fact]
+    public void ImportNumberListCallsImportsFile()
+    {
+
+    }
+
+    [Fact]
+    public void ImportNumberListWrongFileFormatThrowsException()
+    {
+
+    }
+
+    [Fact]
+    public void ImportNumberListSerializesFile()
+    {
+
+    }
+
+    [Fact]
+    public void ImportNumberListFileReturnsOrderedList()
+    {
+
+    }
+
+    [Fact]
+    public void ImportNumberListFileCallsPerfomanceMeasurement()
+    {
+
+    }
+
+    // For public List<int> ImportNumberList(List<int> numberList, string fileName)
+    [Fact]
+    public void ImportNumberListReturnsOrderedList()
+    {
+
+    }
+
+    [Fact]
+    public void ImportNumberListCallsPerfomanceMeasurement()
+    {
+
+    }
+
+    [Fact]
+    public void ImportNumberListCallsSaveToFile()
+    {
+
+    }
+
+    [Fact]
+    public void LoadLatestFileCallsLoadFile()
+    {
+
+    }
+}
--- NumberOrdering.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace NumberOrdering.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
